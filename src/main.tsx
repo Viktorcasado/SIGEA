5e9fc6d71362bc4b145f929634e4348b644b896e
[dyad] <viktorcasado@gmail.com>
2026-02-19 17:01:32 -0300

    [dyad] Transformando a aplicação em um PWA instalável - wrote 5 file(s), added vite-plugin-pwa package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from './contexts/ThemeContext';
 import './index.css';
+import { registerSW } from 'virtual:pwa-register';
+
+// Registra o service worker do PWA
+registerSW({ immediate: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
