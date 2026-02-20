3bff06f3131fade7b54d76ce48558cc6362badc6
[dyad] <viktorcasado@gmail.com>
2026-02-20 01:18:58 -0300

    [dyad] Adicionando botão flutuante do WhatsApp - wrote 2 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import BottomBar from './BottomBar';
+import WhatsAppButton from './WhatsAppButton';
 
 export default function Layout() {
   return (
@@ -7,6 +8,7 @@ export default function Layout() {
       <main className="max-w-4xl mx-auto p-4">
         <Outlet />
       </main>
+      <WhatsAppButton />
       <BottomBar />
     </div>
   );
